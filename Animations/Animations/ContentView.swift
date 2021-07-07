2e556fca1921592bf2f9051d0fd9b0753eecae5e
Xun Ruan <galaxymacos@gmail.com>
2021-07-07 18:01:12 +0800

    Controlling the animation stack

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,101 +9,63 @@ import SwiftUI
 
 
 
-struct ContentView: View {
-    @State var animationValue: CGFloat = 1
+//struct ContentView: View {
+//    @State private var animationAmount = 0.0
+//
+//    var body: some View{
+//        Button("tap me"){
+//            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)){
+//                self.animationAmount += 360
+//            }
+//
+//            withAnimation(){
+//                self.animationAmount += 360
+//            }
+//        }
+//        .padding(50)
+//        .foregroundColor(.white)
+//        .background(Color.red)
+//        .clipShape(Circle())
+//        .rotation3DEffect(
+//            .degrees(animationAmount),
+//            axis: (x: 0.0, y: 1.0, z: 0.0)
+//        )
+//    }
+//}
 
-    struct RedRoundButton: View{
-        @Binding var animationValue: CGFloat
-        var body: some View{
-            Button("Tap me"){
-                animationValue += 1
-            }
-            .padding(50)
-            .foregroundColor(.white)
-            .background(Color.accentColor)
-            .clipShape(Circle())
-            .scaleEffect(animationValue)
-        }
-    }
-    
-    struct RedRoundButtonWithoutScalingEffect: View{
-        @Binding var animationValue: CGFloat
-        var body: some View{
-            Button("Tap me"){
-                animationValue += 1
-            }
-            .padding(50)
-            .foregroundColor(.white)
-            .background(Color.accentColor)
-            .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(Color.red)
-                    .scaleEffect(animationValue)
-                    .opacity(Double(2 - animationValue))
-            )
-        }
-    }
-    
-    struct OverlayEffect: ViewModifier{
-        @Binding var animationValue: CGFloat
-        func body(content: Content) -> some View {
-            content
-                .overlay(
-                    Circle()
-                        .stroke(Color.red)
-                        .scaleEffect(animationValue)
-                        .opacity(Double(2 - animationValue))
-                        .animation(
-                            Animation.easeOut(duration: 1)
-                                .repeatForever(autoreverses: false)
-                        )
-                )
-        }
-    }
-    
-    struct ScalingEffect: ViewModifier{
-        @Binding var animationValue: CGFloat
-        func body(content: Content) -> some View {
-            content
-                .scaleEffect()
-        }
-    }
+// Controlling the animation stack
+struct ContentView: View {
+    @State var darkMode = false
     
     var body: some View{
         VStack{
-//            RedRoundButton(animationValue: $animationValue.animation())
-            
-//            RedRoundButton(animationValue: $animationValue.animation(.easeOut))
-            
-//            RedRoundButton(animationValue: $animationValue.animation(.interpolatingSpring(stiffness: 50, damping: 1)))
-            
-//            RedRoundButton(animationValue: $animationValue.animation(.easeInOut(duration: 0.25)))
-            
-//            RedRoundButton(animationValue: $animationValue.animation(.easeInOut(duration: 0.25).delay(1)))
-            
-//            RedRoundButton(animationValue: $animationValue.animation(.easeInOut(duration: 0.25).repeatCount(3, autoreverses: true)))
-            
-//            RedRoundButton(animationValue: $animationValue.animation(.easeInOut(duration: 0.25).repeatForever(autoreverses: false)))
-            
-//            RedRoundButton(animationValue: $animationValue.animation())
-            
-//            RedRoundButtonWithoutScalingEffect(animationValue: $animationValue.animation())
-            
-            // TODO figure out why it can't auto loop the animation on startup
-//            RedRoundButtonWithoutScalingEffect(animationValue: $animationValue.animation(.easeOut(duration: 1).repeatForever(autoreverses: false)))
-            
-            
-//            RedRoundButton(animationValue: $animationValue.animation()).modifier(OverlayEffect(animationValue: $animationValue))
+            Button("Tap"){
+                darkMode.toggle()
+            }
+            .padding(50)
+            .background(darkMode ? Color.black : Color.red)
+            // You can disable animation so that the modifier above this animation has no animation
+            .animation(nil)
+            .foregroundColor(darkMode ? .black : .white)
+            .clipShape(Circle())
+            .scaleEffect(darkMode ? 2 : 1)
+            .animation(.default)
+            .rotation3DEffect(
+                .degrees(darkMode ? 180: 0),
+                axis: (x: 0.0, y: 1.0, z: 0.0)
+            )
+            .animation(.easeOut)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
     }
 }
 
 
 
+//
