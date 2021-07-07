eb7fd0b3ac028af97d4c12ecdaca9b2a70aa4ebe
Xun Ruan <galaxymacos@gmail.com>
2021-07-07 14:14:53 +0800

    Try out different kinds of animations

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,34 +7,94 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
-    struct MyButton: View {
-        @Binding var animationAmount: CGFloat
+    @State var animationValue: CGFloat = 1
+
+    struct RedRoundButton: View{
+        @Binding var animationValue: CGFloat
         var body: some View{
-            print(animationAmount)
-            return VStack{
-                Button("Tap me"){
-                    self.animationAmount += 1
-                }
-                .padding(40)
-                .background(Color.red)
-                .foregroundColor(.white)
-                .clipShape(Circle())
-                // Change scaleEffect() without animation() will not trigger animation
-                .scaleEffect(animationAmount)
-//                .animation(
-//                            Animation.easeInOut(duration: 1).repeatCount(3, autoreverses: true))
+            Button("Tap me"){
+                animationValue += 1
             }
-            
+            .padding(50)
+            .foregroundColor(.white)
+            .background(Color.accentColor)
+            .clipShape(Circle())
+            .scaleEffect(animationValue)
         }
     }
     
-    var body: some View {
-        VStack {
-            Stepper("Scale amount", value: $animationAmount, in: 1...10)
-
-            MyButton(animationAmount: $animationAmount)
+    struct RedRoundButtonWithoutScalingEffect: View{
+        @Binding var animationValue: CGFloat
+        var body: some View{
+            Button("Tap me"){
+                animationValue += 1
+            }
+            .padding(50)
+            .foregroundColor(.white)
+            .background(Color.accentColor)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(Color.red)
+                    .scaleEffect(animationValue)
+                    .opacity(Double(2 - animationValue))
+            )
+        }
+    }
+    
+    struct OverlayEffect: ViewModifier{
+        @Binding var animationValue: CGFloat
+        func body(content: Content) -> some View {
+            content
+                .overlay(
+                    Circle()
+                        .stroke(Color.red)
+                        .scaleEffect(animationValue)
+                        .opacity(Double(2 - animationValue))
+                        .animation(
+                            Animation.easeOut(duration: 1)
+                                .repeatForever(autoreverses: false)
+                        )
+                )
+        }
+    }
+    
+    struct ScalingEffect: ViewModifier{
+        @Binding var animationValue: CGFloat
+        func body(content: Content) -> some View {
+            content
+                .scaleEffect()
+        }
+    }
+    
+    var body: some View{
+        VStack{
+//            RedRoundButton(animationValue: $animationValue.animation())
+            
+//            RedRoundButton(animationValue: $animationValue.animation(.easeOut))
+            
+//            RedRoundButton(animationValue: $animationValue.animation(.interpolatingSpring(stiffness: 50, damping: 1)))
+            
+//            RedRoundButton(animationValue: $animationValue.animation(.easeInOut(duration: 0.25)))
+            
+//            RedRoundButton(animationValue: $animationValue.animation(.easeInOut(duration: 0.25).delay(1)))
+            
+//            RedRoundButton(animationValue: $animationValue.animation(.easeInOut(duration: 0.25).repeatCount(3, autoreverses: true)))
+            
+//            RedRoundButton(animationValue: $animationValue.animation(.easeInOut(duration: 0.25).repeatForever(autoreverses: false)))
+            
+//            RedRoundButton(animationValue: $animationValue.animation())
+            
+//            RedRoundButtonWithoutScalingEffect(animationValue: $animationValue.animation())
+            
+            // TODO figure out why it can't auto loop the animation on startup
+//            RedRoundButtonWithoutScalingEffect(animationValue: $animationValue.animation(.easeOut(duration: 1).repeatForever(autoreverses: false)))
+            
+            
+//            RedRoundButton(animationValue: $animationValue.animation()).modifier(OverlayEffect(animationValue: $animationValue))
         }
     }
 }
@@ -44,3 +104,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
