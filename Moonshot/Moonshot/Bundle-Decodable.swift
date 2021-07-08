f1aa522eee73bd96869d050da66fbe52dccaea22
Xun Ruan <galaxymacos@gmail.com>
2021-07-08 23:32:11 +0800

    Use generic to load any kind of Codable data

---
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,75 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Xun Ruan on 2021/7/8.
+//
+
+import Foundation
+
+extension Bundle{
+    
+    /*  Replaced by generic
+     // from a file name, get the astronaut info list
+     func decode(_ file: String) -> [Astronaut] {
+         // Imagine url contains all the data
+         guard let url = self.url(forResource: file, withExtension: nil) else {
+             fatalError("Fail to locate \(file) on the disk")
+         }
+
+         // Codable uses Data we are instead going to use Data(contentsOf:)
+         guard let data = try? Data(contentsOf: url) else{
+             fatalError("Fail to load \(file)")
+         }
+
+         let decoder = JSONDecoder()
+         guard let astronauts = try? decoder.decode([Astronaut].self, from: data) else{
+             fatalError("Fail to decode \(file) from bundle.")
+         }
+
+         return astronauts
+     }
+     */
+    
+    
+    
+    /*  Replaced by generic
+     func decodeMission(_ file:String) -> [Mission]{
+     guard let url = self.url(forResource: file, withExtension: nil) else{
+     fatalError("Fail to locate \(file) on the disk")
+     }
+     
+     guard let data = try? Data(contentsOf: url) else{
+     fatalError("Fail to load \(file)")
+     }
+     
+     let decoder = JSONDecoder()
+     guard let decoded = try? decoder.decode([Mission].self, from: data) else{
+     fatalError("Fail to decode \(file) from bundle")
+     }
+     
+     return decoded
+     }
+     
+     */
+    
+    func decode<T: Codable>(_ file: String) -> T {
+        // Imagine url contains all the data
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Fail to locate \(file) on the disk")
+        }
+
+        // Codable uses Data we are instead going to use Data(contentsOf:)
+        guard let data = try? Data(contentsOf: url) else{
+            fatalError("Fail to load \(file)")
+        }
+
+        let decoder = JSONDecoder()
+        guard let astronauts = try? decoder.decode(T.self, from: data) else{
+            fatalError("Fail to decode \(file) from bundle.")
+        }
+
+        return astronauts
+    }
+    
+}
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -42,48 +42,130 @@ struct ContentView: View {
 }
  */
 
-struct ContentView: View {
-    
-    struct CustomText: View {
-        @State var text: String
-        
-        var body: some View{
-            Text("\(text)")
-        }
-        
-        init(text: String) {
-            print("Creating a new custom text")
-            self.text = text
-        }
-    }
-    
-    var body: some View{
-        // ScrollView creates all views at one time
-        ScrollView(.vertical){
-            VStack(spacing: 10){
-                ForEach(1..<100){
-                    CustomText(text: "Item \($0)")
-                        .font(.title)
-                }
-            }
-            /* List creates View only when necessary
-             List{
-             ForEach(1..<100){
-             CustomText(text: "Item \($0)")
-             .font(.title)
-             }
-             */
-            
-            // Make the whole area scrollable
-            .frame(maxWidth: .infinity)
-        }
-    
-        //
-        
-        
-        }
-    }
+/*  ScrollView
+ struct ContentView: View {
+ 
+ struct CustomText: View {
+ @State var text: String
+ 
+ var body: some View{
+ Text("\(text)")
+ }
+ 
+ init(text: String) {
+ print("Creating a new custom text")
+ self.text = text
+ }
+ }
+ 
+ var body: some View{
+ // ScrollView creates all views at one time
+ ScrollView(.vertical){
+ VStack(spacing: 10){
+ ForEach(1..<100){
+ CustomText(text: "Item \($0)")
+ .font(.title)
+ }
+ }
+ /* List creates View only when necessary
+ List{
+ ForEach(1..<100){
+ CustomText(text: "Item \($0)")
+ .font(.title)
+ }
+ */
+ 
+ // Make the whole area scrollable
+ .frame(maxWidth: .infinity)
+ }
+ 
+ //
+ 
+ 
+ }
+ }
+ 
+ */
 
+/*  NavigationView
+ struct ContentView: View{
+ var body: some View{
+ NavigationView{
+ VStack{
+ NavigationLink(destination: Text("detail view")){
+ Text("text")
+ }
+ // List with NavigationLink will make an indicator
+ List(0..<100){ row in
+ NavigationLink(destination: Text("Detail \(row)")){
+ Text("Row \(row)")
+ }
+ }
+ }
+ .navigationTitle("SwiftUI")
+ }
+ }
+ }
+ 
+ */
+
+/* Working with hierarchical codable data
+ struct ContentView: View{
+ struct Address:Codable {
+ var street: String
+ var city: String
+ }
+ 
+ struct User:Codable {
+ var name: String
+ var address: Address
+ }
+ 
+ var body: some View{
+ Button("Decode JSON"){
+ let input = """
+ {
+ "name": "Taylor Swift",
+ "address": {
+ "street": "555, Taylor Swift Avenue",
+ "city": "Nashville"
+ }
+ }
+ """
+ let data = Data(input.utf8)
+ let decoder = JSONDecoder()
+ if let user = try? decoder.decode(User.self, from: data){
+ print(user.address.street)
+ print(user.address.city)
+ print(user.name)
+ }
+ }
+ }
+ 
+ }
+ 
+ */
+
+/*  Loading a specific kind of codable data
+ let astronauts = Bundle.main.decode("astronauts.json")
+ 
+ struct ContentView: View{
+ var body: some View{
+ Text("\(astronauts.count) astronauts found")
+ }
+ }
+ 
+ */
+
+struct ContentView: View{
+    var astronauts:[Astronaut] = Bundle.main.decode("astronauts.json")
+    var missions:[Mission] = Bundle.main.decode("missions.json")
+    var body: some View{
+        Text("\(astronauts.count) astronauts to \(missions.count) missions")
+    }
+    
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
