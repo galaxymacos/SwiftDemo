f483a27278e6a288ccde81b76cd8ad0f6209ced0
Xun Ruan <galaxymacos@gmail.com>
2021-07-12 23:14:04 +0800

    Fix a typo

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
             #endif
 
             Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")s
+                Label("Add Item", systemImage: "plus")
             }
         }
     }
