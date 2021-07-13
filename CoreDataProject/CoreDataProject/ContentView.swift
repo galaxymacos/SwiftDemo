600f051992ce4cd6c1f2019c3131748d3902a240
Xun Ruan <galaxymacos@gmail.com>
2021-07-13 22:32:09 +0800

    Conditional saving of NSManagedObjectContext

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    
     var body: some View{
         Text("text")
+    }
+    
+    func safeSave(){
+        if moc.hasChanges{
+            try? moc.save()
+        }
     }
 }
 
