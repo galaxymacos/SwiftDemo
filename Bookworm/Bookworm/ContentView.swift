0ca6dc760c08e0cb2775a482669d08956f3f51f6
Xun Ruan <galaxymacos@gmail.com>
2021-07-13 08:38:08 +0800

    Set up two ways binding

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -10,7 +10,7 @@ import CoreData
 
 struct PushButton: View {
     var title = "Push"
-    @State var isOn = false
+    @Binding var isOn:Bool
     var onColor = [Color.red, Color.yellow]
     var offColor = [Color(white: 0.6), Color(white: 0.4)]
     
@@ -21,67 +21,76 @@ struct PushButton: View {
         .padding()
         .background(LinearGradient(gradient: isOn ? Gradient(colors: [onColor[0], onColor[1]]):Gradient(colors: [offColor[0], offColor[1]]), startPoint: .top, endPoint: .bottom))
         .foregroundColor(.white)
-        clipShape(Capsule())
-            .shadow(radius: isOn ? 0 : 5)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
     }
 }
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    /* Unknown core data stuff
+     @Environment(\.managedObjectContext) private var viewContext
+     
+     @FetchRequest(
+     sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+     animation: .default)
+     private var items: FetchedResults<Item>
+     
+     var body: some View {
+     List {
+     ForEach(items) { item in
+     Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+     }
+     .onDelete(perform: deleteItems)
+     }
+     .toolbar {
+     #if os(iOS)
+     EditButton()
+     #endif
+     
+     Button(action: addItem) {
+     Label("Add Item", systemImage: "plus")
+     }
+     }
+     */
+    
+    @State var rememberMe: Bool = false
+    var body: some View{
+        VStack{
+            PushButton(title: "Remember me?", isOn: $rememberMe)
+            Text("\(rememberMe ? "On":"Off")")
         }
     }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(context: viewContext)
+//            newItem.timestamp = Date()
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
+//
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            offsets.map { items[$0] }.forEach(viewContext.delete)
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
 }
 
 private let itemFormatter: DateFormatter = {
