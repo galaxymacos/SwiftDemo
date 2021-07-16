0a746bb7df9b8bf58d203b7cded3aa6f7a23c231
Xun Ruan <xunruan@icloud.com>
2021-07-16 19:48:08 +0800

    Using TouchID and FaceID with SwiftUI

--- BucketList/Shared/ContentView.swift
+++ BucketList/Shared/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 /* Adding conformance to Comparable for custom types
  struct User: Identifiable, Comparable {
@@ -108,10 +109,54 @@ import SwiftUI
  }
  */
 
-struct ContentView: View {
+/* Communicating with a MapKit coordinator
+ struct ContentView: View {
+ @State var isUnlocked = false
+ var body: some View{
+ MapView()
+ .edgesIgnoringSafeArea(.all)
+ }
+ }
+ */
+
+
+struct ContentView: View{
+    @State var isUnlocked = false
     var body: some View{
-        MapView()
-            .edgesIgnoringSafeArea(.all)
+        VStack{
+            if isUnlocked{
+                Text("Unlocked")
+            }
+            else{
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                DispatchQueue.main.async {
+                    if success {
+                        isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
+        
     }
 }
 
