b86d60cffe1de9f07181851b32d58dbca1cb2908
Xun Ruan <xunruan@icloud.com>
2021-07-16 16:45:17 +0800

    Communicating with a MapKit coordinator

--- BucketList/Shared/MapView.swift
+++ BucketList/Shared/MapView.swift
@@ -11,6 +11,14 @@ import MapKit
 struct MapView: UIViewRepresentable {
     func makeUIView(context: Context) -> some UIView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Capital of London"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
+        mapView.addAnnotation(annotation)
+        
         return mapView
     }
     
@@ -18,7 +26,28 @@ struct MapView: UIViewRepresentable {
         
     }
     
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
     
+    class Coordinator: NSObject, MKMapViewDelegate {
+        let parent: MapView
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            print(mapView.centerCoordinate)
+        }
+        
+        // change the pin
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)    // reuse it to save resource
+            // allow user to tap the pin to show information
+            view.canShowCallout = true
+            return view
+        }
+    }
 }
 
 struct MapView_Previews: PreviewProvider {
