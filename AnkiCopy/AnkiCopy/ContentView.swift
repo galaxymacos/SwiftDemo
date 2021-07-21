b328b31c6da3b542600acd42339d45f51b40b5e8
Xun Ruan <xunruan@icloud.com>
2021-07-21 21:37:26 +0800

    Building a stack of cards

--- AnkiCopy/AnkiCopy/ContentView.swift
+++ AnkiCopy/AnkiCopy/ContentView.swift
@@ -7,10 +7,29 @@
 
 import SwiftUI
 
+extension View{
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = total - position
+        return self.offset(x: 0, y: CGFloat(offset * 10))
+    }
+}
 
 struct ContentView: View {
+    @State var cards = [Card](repeating: Card.example, count: 10)
+    
+    
     var body: some View {
-        CardView(card: Card.example)
+        ZStack{
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            
+            ForEach(0..<cards.count, id: \.self){ index in
+                CardView(card: Card.example)
+                    .stacked(at: index, in: cards.count)
+            }
+        }
     }
 }
 
