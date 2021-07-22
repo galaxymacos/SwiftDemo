6b0e1319739c8ca1f3b2cdd01f677435f92d7fad
Xun Ruan <xunruan@icloud.com>
2021-07-22 11:45:37 +0800

    Ending the app with allowsHitTesting()

--- AnkiCopy/AnkiCopy/CardView.swift
+++ AnkiCopy/AnkiCopy/CardView.swift
@@ -8,29 +8,62 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
     @State var card: Card
     @State var isShowingAnswer = false
     @State var offset = CGSize.zero
+   
     var removal: (()->Void)? = nil
     var body: some View {
-        ZStack{
-            RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.white)
-                .shadow(radius: 10)
-            
-            VStack{
-                Text(card.prompt)
+        VStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .fill(differentiateWithoutColor
+                            ? Color.white.opacity(1 - Double(abs(offset.width / 50)))
+                            : Color.white
+                    )
+                    .shadow(radius: 10)
+                    .background(differentiateWithoutColor
+                                    ? nil
+                                    : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                    .fill(offset.width>0 ? Color.green : Color.red))
+                
+                VStack{
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
+                    
+                }
+                .padding(20)
+                .multilineTextAlignment(.center)
+                VStack{
+                    Spacer()
+                    HStack{
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(Color.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                    .foregroundColor(.white)
                     .font(.largeTitle)
-                    .foregroundColor(.black)
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.gray)
+                    .padding()
                 }
             }
-            .padding(20)
-            .multilineTextAlignment(.center)
+            
+            
         }
+        
         .frame(width: 450, height: 250)
         .rotationEffect(.degrees(Double(self.offset.width / 5)))
         .offset(x:self.offset.width * 5, y:0)
@@ -50,6 +83,7 @@ struct CardView: View {
         .onTapGesture {
             isShowingAnswer.toggle()
         }
+        
     }
 }
 
--- AnkiCopy/AnkiCopy/ContentView.swift
+++ AnkiCopy/AnkiCopy/ContentView.swift
@@ -16,29 +16,81 @@ extension View{
 
 struct ContentView: View {
     @State var cards = [Card](repeating: Card.example, count: 10)
-    
+    @State var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var isActive = true
+    @State var timeRemaining = 100
     
     
     var body: some View {
         ZStack{
+            // Background Image
             Image("background")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
-            
-            ForEach(0..<cards.count, id: \.self){ index in
-                CardView(card: Card.example){
-                    withAnimation{
-                        removeCard(index: index)
+            VStack{
+                // Timer
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color.black)
+                            .opacity(0.7)
+                    )
+                // Card
+                ZStack{
+                    ForEach(0..<cards.count, id: \.self){ index in
+                        CardView(card: Card.example, removal: {
+                            withAnimation{
+                                removeCard(index: index)
+                            }
+                        })
+                        .stacked(at: index, in: cards.count)
                     }
+                    
+                }.allowsHitTesting(timeRemaining>0)
+                
+                if cards.isEmpty{
+                    Button("Start again", action: resetCards)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.black)
+                        .clipShape(Capsule())
                 }
-                    .stacked(at: index, in: cards.count)
             }
+            
         }
+        .onReceive(timer, perform: { _ in
+            // Stop timer from running in the background
+            guard isActive else {return}
+            if(timeRemaining>0){
+                timeRemaining -= 1
+            }
+        })
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification), perform: { _ in
+            isActive = false
+        })
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.didEnterBackgroundNotification), perform: { _ in
+            if !cards.isEmpty{                
+                isActive = true
+            }
+        })
     }
     
     func removeCard(index: Int) {
         cards.remove(at: index)
+        if cards.isEmpty{
+            isActive = false
+        }
+    }
+    
+    func resetCards(){
+        cards = [Card](repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
