992e5787e8a891dec986ecf1191d6bd2599e017b
Xun Ruan <xunruan@icloud.com>
2021-07-25 00:02:27 +0800

    Absolute positioning with SwiftUI views

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -59,40 +59,53 @@ import SwiftUI
  */
 
 /* Custom alignment guide
+ struct ContentView: View{
+ var body: some View{
+ HStack(alignment: .midAccountAndName) {
+ VStack {
+ Text("@twostraws")
+ .alignmentGuide(.midAccountAndName){d in d[VerticalAlignment.center]
+ }
+ Image("paul-hudson")
+ .resizable()
+ .frame(width: 64, height: 64)
+ }
+ 
+ VStack {
+ Text("Full name:")
+ Text("PAUL HUDSON")
+ .font(.largeTitle)
+ .alignmentGuide(.midAccountAndName){d in d[VerticalAlignment.center]
+ }
+ }
+ }
+ }
+ }
+ 
+ extension VerticalAlignment {
+ enum MidAccountAndName: AlignmentID {
+ static func defaultValue(in d: ViewDimensions) -> CGFloat {
+ d[.top]
+ }
+ }
+ 
+ static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+ }
  */
 
 struct ContentView: View{
     var body: some View{
-        HStack(alignment: .midAccountAndName) {
-                    VStack {
-                        Text("@twostraws")
-                            .alignmentGuide(.midAccountAndName){d in d[VerticalAlignment.center]
-                            }
-                        Image("paul-hudson")
-                            .resizable()
-                            .frame(width: 64, height: 64)
-                    }
-
-                    VStack {
-                        Text("Full name:")
-                        Text("PAUL HUDSON")
-                            .font(.largeTitle)
-                            .alignmentGuide(.midAccountAndName){d in d[VerticalAlignment.center]
-                            }
-                    }
-                }
+//        Text("Hello world")
+//            .position(x: 0, y: 0)
+//            .background(Color.red)
+        
+        Text("Hello world")
+            // offset won't change its actual position
+            .offset(x: 100, y: 100)
+            .background(Color.red)
     }
 }
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            d[.top]
-        }
-    }
-
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
