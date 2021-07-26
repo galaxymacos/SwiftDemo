79411220867211b1d78d72fc374b603277c01c0e
Xun Ruan <xunruan@icloud.com>
2021-07-26 10:56:59 +0800

    Use alert() and sheet() with optional

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -18,7 +18,9 @@ struct NavigationviewDifferentBehavioriPhoneiPad: View {
     }
 }
 
-struct ContentView: View {
+
+
+struct SplitView_NavigationLink: View {
     var body: some View{
         NavigationView{
             NavigationLink(destination: Text("Second text")){
@@ -29,6 +31,32 @@ struct ContentView: View {
             // This won't be shown
             Text("Second View")
         }
+    }
+}
+
+struct Test_SheetAlert_Optional: View {
+    
+    struct User: Identifiable{
+        var id = "Taylor swift"
+    }
+    
+    @State var selectedUser:User? = nil
+    var body: some View{
+        Text("Hello world")
+            .onTapGesture {
+                selectedUser = User()
+            }
+            // it will automatically unwrap the optional value (You don't have to handle the force wrapping
+            .alert(item: $selectedUser){user in
+                Alert(title: Text("title"), message: Text("message"), dismissButton: .default(Text("Dismiss")))
+            }
+        
+    }
+}
+
+struct ContentView: View {
+    var body: some View{
+        Test_SheetAlert_Optional()
     }
 }
 
