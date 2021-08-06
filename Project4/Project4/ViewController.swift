8dfa95743695be0aa5d2306b246c4d9bcbe9cfb3
Xun Ruan <galaxymacos@gmail.com>
2021-08-06 16:10:55 +0800

    Connect table view to navigation view

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -9,31 +9,37 @@ import UIKit
 import WebKit
 
 // UIViewController is the class we inherit, and WKNavigationDelegate is the protocol we conform to
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UITableViewController, WKNavigationDelegate {
 
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com", "google.com", "baidu.com"]
     
-    override func loadView() {
-        webView = WKWebView()   // Create a WKWebView()
-        webView.navigationDelegate = self   // tell UIKit this class can handle the navigation function
-        view = webView  // set it here or it is too late
-    }
+//    override func loadView() {
+//        webView = WKWebView()   // Create a WKWebView()
+//        webView.navigationDelegate = self   // tell UIKit this class can handle the navigation function
+//        view = webView  // set it here or it is too late
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Websites"
         // Do any additional setup after loading the view.
-        let url = URL(string: "https://www.hackingwithswift.com")!  // URL(string) will create an url?
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress"{
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
     
     @objc func openTapped(){
         // Action sheet is created using UIAlertController, and option is added using UIAlertAction
         let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -49,5 +55,66 @@ class ViewController: UIViewController, WKNavigationDelegate {
         // title means the navigation bar title
         title = webView.title
     }
+    
+    // Decides whether we allows or refuses a visit to a site
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host{
+            for website in websites {
+                if host.contains(website){
+                    decisionHandler(.allow)
+                    print("allow")
+                    return
+                }
+            }
+        }
+        if url != nil{
+            print(url)
+            let ac = UIAlertController(title: "Block", message: "Not allow to visit", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(ac, animated: true, completion: nil)
+            decisionHandler(.cancel)
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return websites.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "website", for: indexPath)
+        cell.textLabel?.text = websites[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        webView = WKWebView()   // Create a WKWebView()
+        webView.navigationDelegate = self   // tell UIKit this class can handle the navigation function
+        view = webView  // set it here or it is too late
+        let url = URL(string: "https://"+websites[indexPath.row])!  // URL(string) will create an url?
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()    // only take up the space of its content
+        let progressButton = UIBarButtonItem(customView: progressView)  // Wrap up UIProgressView in a UIBarButtonItem so that it can go into our toolbar
+        
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let goback = UIBarButtonItem(barButtonSystemItem: .cancel, target: webView, action: #selector(webView.goBack))
+        let goForward = UIBarButtonItem(barButtonSystemItem: .fastForward, target: webView, action: #selector(webView.goForward))
+        toolbarItems = [goback, progressButton, spacer, refresh, goForward]
+        
+        navigationController?.isToolbarHidden = false
+        
+        // #keypath let complier to check your code is right, that WKWebView actually has the estimatedProgress property
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
 }
 
