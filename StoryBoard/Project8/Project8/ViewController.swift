53039edcd367cc2990da14af1bea3aa5e5266dd1
Xun Ruan <galaxymacos@gmail.com>
2021-08-10 15:12:26 +0800

    firstIndex(of:) and joined()

--- StoryBoard/Project8/Project8/ViewController.swift
+++ StoryBoard/Project8/Project8/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     var currentAnswer: UITextField!
     var letterButtons = [UIButton]()
     
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
+    
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
@@ -79,11 +85,13 @@ class ViewController: UIViewController {
         let submit = UIButton(type: .system)
         submit.translatesAutoresizingMaskIntoConstraints = false
         submit.setTitle("SUBMIT", for: .normal)
+        submit.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
         view.addSubview(submit)
         
         let clear = UIButton(type: .system)
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("Clear", for: .normal)
+        clear.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
         view.addSubview(clear)
         
         NSLayoutConstraint.activate([
@@ -116,6 +124,7 @@ class ViewController: UIViewController {
         for row in 0..<4{
             for col in 0..<5{
                 let letterButton = UIButton(type: .system)
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
                 letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 36)
                 
                 // give the button some temporary text so we can see it on-screen
@@ -136,8 +145,94 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadLevel()
     }
 
+    @objc func letterTapped(_ sender: UIButton){
+        guard let buttonTitle = sender.titleLabel?.text else {return}
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
+    }
+    
+    @objc func submitTapped(_ sender: UIButton){
+        guard let answerText = currentAnswer.text else {return} // Make sure there is text in label
+        
+        if let solutionPosition = solutions.firstIndex(of: answerText){
+            activatedButtons.removeAll()
+            
+            var splitAnswers = answersLabel.text?.components(separatedBy: "\n")
+            splitAnswers?[solutionPosition] = answerText    // Change the solution from showing "9 letters" to an actual word
+            answersLabel.text = splitAnswers?.joined(separator: "\n")
+            
+            currentAnswer.text = ""
+            score += 1
+            
+            if score % 7 == 0{
+                let ac = UIAlertController(title: "Well done!", message: "Are you ready for th next level?", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Let's go!", style: .default, handler: levelUp))
+                present(ac, animated: true)
+            }
+        }
+    }
+    
+    func levelUp(action: UIAlertAction){
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
+        
+        loadLevel()
+        
+        for btn in letterButtons{
+            btn.isHidden = false
+        }
+    }
+    
+    @objc func clearTapped(_ sender: UIButton){
+        currentAnswer.text = ""
+        for btn in activatedButtons{
+            btn.isHidden = false
+        }
+        activatedButtons.removeAll()
+    }
+    
+    func loadLevel(){
+        var clueString = "" // This string will store all the clues
+        var solutionString = "" // This string will store "how many letters" in all solutions
+        var letterBits = [String]() // This array will store "XJS", "AIX" ... into all the buttons
+        
+        let url = Bundle.main.url(forResource: "level\(level)", withExtension: "txt")!
+        if let urlContent = try? String(contentsOf: url){
+            var lines = urlContent.components(separatedBy: "\n")
+            lines.shuffle()
+            
+            for (index, line) in lines.enumerated(){
+                let parts = line.components(separatedBy: ": ")
+                let answer = parts[0]
+                let clue = parts[1]
+                
+                clueString += "\(index+1). \(clue)\n"
+                
+                let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                solutionString = "\(solutionWord.count) letters\n"
+                solutions.append(solutionWord)
+                
+                let bits = answer.components(separatedBy: "|")
+                letterBits += bits
+            }
+        }
+        
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        letterBits.shuffle()
+        
+        if letterBits.count == letterButtons.count{
+            for i in 0..<letterButtons.count{
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+        
+    }
     
 }
 
