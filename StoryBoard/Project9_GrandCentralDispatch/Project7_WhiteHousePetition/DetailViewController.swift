8201a9c1f1a24c01251770c4f521916a7b0721c6
Xun Ruan <galaxymacos@gmail.com>
2021-08-11 10:12:26 +0800

    Add GCD to project 7 and call it project 9
    DispatchQueue.main.async
    
    DispatchQueue.global(qos: .userInitiated).async{}

---
+++ StoryBoard/Project9_GrandCentralDispatch/Project7_WhiteHousePetition/DetailViewController.swift
@@ -0,0 +1,55 @@
+//
+//  DetailViewController.swift
+//  DetailViewController
+//
+//  Created by Xun Ruan on 2021/8/9.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        guard let detailItem = detailItem else {
+            return
+        }
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StoryBoard/Project9_GrandCentralDispatch/Project7_WhiteHousePetition/ViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  Project7_WhiteHousePetition
+//
+//  Created by Xun Ruan on 2021/8/9.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+    var originalPetitions = [Petition]()
+    var isFilterOn:Bool = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0{
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        }
+        else{
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        originalPetitions = petitions
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self?.parse(json: data)
+                }
+                else{
+                    self?.showError()
+                }
+            }
+            else{
+                self?.showError()
+            }
+        }
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showCredit))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(filterPetition))
+    }
+    
+    func parse(json: Data){
+        if let decoded = try? JSONDecoder().decode(Petitions.self, from: json){
+            petitions = decoded.results
+            
+            DispatchQueue.main.async {[weak self] in  // Push the code that executes on the background back to the main queue
+                self?.tableView.reloadData()
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = petitions[indexPath.row].title
+        cell.detailTextLabel?.text = petitions[indexPath.row].body
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let detailvc = DetailViewController()
+        detailvc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(detailvc, animated: true)
+    }
+    
+    func showError(){
+        DispatchQueue.main.async {[weak self] in
+            let ac = UIAlertController(title: "Error", message: "Please check your connection", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self?.present(ac, animated: true)
+            
+        }
+    }
+    
+    @objc func showCredit(){
+        let ac = UIAlertController(title: "Credits", message: "We the people API of the whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func filterPetition(){
+        if !self.isFilterOn{
+            let ac = UIAlertController(title: "Filter", message: "Add string to filter", preferredStyle: .alert)
+            ac.addTextField(configurationHandler: nil)
+            ac.addAction(UIAlertAction(title: "Submit", style: .default){[weak self, weak ac] _ in
+                self?.originalPetitions = self!.petitions
+                    let filterString = ac?.textFields?[0].text ?? ""
+                    print(filterString)
+                    self?.petitions = self?.petitions.filter{$0.title.contains(filterString)} ?? [Petition]()
+                    self?.tableView.reloadData()
+                    self?.isFilterOn.toggle()
+                    
+                
+            })
+            
+            present(ac, animated: true)
+        }
+        else{
+            self.petitions = self.originalPetitions
+            self.tableView.reloadData()
+            self.isFilterOn.toggle()
+        }
+    }
+}
+
