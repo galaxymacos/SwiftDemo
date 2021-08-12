c1561b732c79d9fae48daeca3735aed43ec4e8f8
Xun Ruan <galaxymacos@gmail.com>
2021-08-12 08:22:04 +0800

    Use UICollectionView to create cells

---
+++ StoryBoard/Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  PersonCell
+//
+//  Created by Xun Ruan on 2021/8/12.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
